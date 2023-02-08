7cb9bada7a15da0d0b62f92d4a810617e328f18b
bikesh bade <36373140+bikesbade@users.noreply.github.com>
2023-02-08 12:45:33 +0400

    landing page update

--- geodata/settings.py
+++ geodata/settings.py
@@ -15,10 +15,10 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = False
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','geotechx.com','217.160.55.177','13.231.246.37']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','geotechx.com','217.160.55.177','13.231.246.37','orycta.com']
 
 # Application definition
 
