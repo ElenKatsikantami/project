fdd8ceae2f6853946a333ac74c9b4ad2ee1b0787
bikesh bade <36373140+bikesbade@users.noreply.github.com>
2023-03-11 14:29:04 +0400

    data table

--- geodata/settings.py
+++ geodata/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ajax_datatable',
     'widget_tweaks',
     'apps.home',
     'apps.authentication'
